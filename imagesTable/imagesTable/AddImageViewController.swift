b45176e511d21cc9071e89dd4f102ebac34d944f
Kwon Byung Soo <planon21@gmail.com>
2017-02-07 13:59:20 +0900

    cell update

--- imagesTable/imagesTable/AddImageViewController.swift
+++ imagesTable/imagesTable/AddImageViewController.swift
@@ -47,18 +47,17 @@ class AddImageViewController: UIViewController,UIImagePickerControllerDelegate,U
         //사용자 사진을 담을 객체 초기화 하여 photoInfo 이름으로 사용
         let photoInfo: PhotoInfo = PhotoInfo()
         
-        
-        
-        
-        
-        
-        
+    
         
         
         
         if let image = self.selectedImage.image{
             
             print("사용자가 선택한 이미지가 있습니다")
+            
+            //사용자가 선택한 이미지를 jpeg data 형식으로 변환하여 photoInfo 에 저장
+            photoInfo.imageData = UIImageJPEGRepresentation(image, 1.0)
+            
         } else {
             
             let alert:UIAlertController
@@ -79,6 +78,10 @@ class AddImageViewController: UIViewController,UIImagePickerControllerDelegate,U
         
         if let title = self.titleField.text, title.characters.count>0 {
             print("사용자가 작성한 제목이 있습니다.")
+            
+            //사용자가 작성한 제목을 저장
+            photoInfo.title = title
+            
         }else {
             let alert:UIAlertController
             alert = UIAlertController(title: "알림", message: "제목을 써주세요.", preferredStyle: UIAlertControllerStyle.alert)
@@ -92,6 +95,19 @@ class AddImageViewController: UIViewController,UIImagePickerControllerDelegate,U
             
             
         }
+        
+        
+        // realm 에게 photoInfo를 DB에 저장해달라고 요청
+        try? realm?.write {
+            realm?.add(photoInfo)
+            
+            //저장을 성공했으면 이전 화면으로 돌아가기
+            self.navigationController?.popViewController(animated: true)
+            
+        }
+        
+        
+        
         
     }
     
--- imagesTable/imagesTable/ViewController.swift
+++ imagesTable/imagesTable/ViewController.swift
@@ -7,9 +7,49 @@
 //
 
 import UIKit
+import RealmSwift
+
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    //스토리보드 테이블뷰와 연결할 아울렛 변수
+    @IBOutlet weak var tableView: UITableView!
+    
+    //데이터베이스에서 불러온 사진정보들을 담아놓을 변수
+    var photoInfos: Results<PhotoInfo>?
+    
+    
+    
+    // ViewController 의 화면이 보여지기 직전에 불려질 함수
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        print("ViewController 의 화면이 보여질 예정입니다.")
+        
+        //Realm 을 초기화하여 realm 이라는 이름으로 사용합니다.
+        let realm = try? Realm()
+        
+        //PhotoInfo 에 해당하는 자료들을 데이터베이스에서 불러와서 photoInfos에 넣어줍니다
+        self.photoInfos = realm?.objects(PhotoInfo.self)
+        
+        //테이블뷰를 새로고침합니다
+        // 테이블 뷰를 새로고침하면 테이블뷰가 보스에게 다시 row 의 개수와 데이터를 요청합니다.
+       
+        self.tableView.reloadData()
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -23,7 +63,9 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        
+        // photoInfos 는 옵셔널이므로 옵셔널에 값이 없으면 0을 돌려주세요
+        return self.photoInfos?.count ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
